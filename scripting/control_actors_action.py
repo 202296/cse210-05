b225806ba8e07d20d31db8ad09753c90b57d7656
milamelo4 <mel19009@byui.edu>
2022-06-18 14:18:05 -0600

    Updated the folder scripting
    Updated the constrol_actors_action, adding a keyboard command for the second snake, and tail increment as they move.
    Updated the handle_collisions_action, game over if player collides with opponents trail.

--- scripting/control_actors_action.py
+++ scripting/control_actors_action.py
@@ -21,6 +21,7 @@ class ControlActorsAction(Action):
         """
         self._keyboard_service = keyboard_service
         self._direction = Point(constants.CELL_SIZE, 0)
+        self._points = 0
 
     def execute(self, cast, script):
         """Executes the control actors action.
@@ -32,18 +33,62 @@ class ControlActorsAction(Action):
         # left
         if self._keyboard_service.is_key_down('a'):
             self._direction = Point(-constants.CELL_SIZE, 0)
+            for i in range(1):
+                snake = cast.get_first_actor("snakes")
+                snake.grow_tail(i + 1)
         
         # right
         if self._keyboard_service.is_key_down('d'):
             self._direction = Point(constants.CELL_SIZE, 0)
+            for i in range(1):
+                snake = cast.get_first_actor("snakes")
+                snake.grow_tail(i + 1)
         
         # up
         if self._keyboard_service.is_key_down('w'):
             self._direction = Point(0, -constants.CELL_SIZE)
+            for i in range(1):
+                snake = cast.get_first_actor("snakes")
+                snake.grow_tail(i + 1)
         
         # down
         if self._keyboard_service.is_key_down('s'):
             self._direction = Point(0, constants.CELL_SIZE)
+            for i in range(1):
+                snake = cast.get_first_actor("snakes")
+                snake.grow_tail(i + 1)
+                
+        # Other command
+        if self._keyboard_service.is_key_down('j'):
+            self._directions = Point(-constants.CELL_SIZE, 0)
+            for i in range(1):
+                snake2 = cast.get_first_actor("foods")
+                snake2.grow_tail(i + 1)
+        
+        # right
+        if self._keyboard_service.is_key_down('l'):
+            self._directions = Point(constants.CELL_SIZE, 0)
+            for i in range(1):
+                snake2 = cast.get_first_actor("foods")
+                snake2.grow_tail(i + 1)
+        
+        # up
+        if self._keyboard_service.is_key_down('i'):
+            self._directions = Point(0, -constants.CELL_SIZE)
+            for i in range(1):
+                snake2 = cast.get_first_actor("foods")
+                snake2.grow_tail(i + 1)
+        
+        # down
+        if self._keyboard_service.is_key_down('k'):
+            self._directions = Point(0, constants.CELL_SIZE) 
+            for i in range(1):
+                snake2 = cast.get_first_actor("foods")
+                snake2.grow_tail(i + 1)    
+                
         
         snake = cast.get_first_actor("snakes")
         snake.turn_head(self._direction)
+        
+        snake2 = cast.get_first_actor("foods")
+        snake2.turn_head(self._directions)
--- scripting/draw_actors_action.py
+++ scripting/draw_actors_action.py
@@ -30,10 +30,13 @@ class DrawActorsAction(Action):
         food = cast.get_first_actor("foods")
         snake = cast.get_first_actor("snakes")
         segments = snake.get_segments()
+        segments1 = food.get_segments()
         messages = cast.get_actors("messages")
 
         self._video_service.clear_buffer()
         self._video_service.draw_actor(food)
+        self._video_service.draw_actors(segments1)
+        self._video_service.draw_actor(snake)
         self._video_service.draw_actors(segments)
         self._video_service.draw_actor(score)
         self._video_service.draw_actors(messages, True)
